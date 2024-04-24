e83490c49dc1afa04da7d510fcfb6b621d336bae
dir-FY24kiyoseakiyu <akiyu.kiyose.g585@dirbato.co.jp>
2024-04-24 11:16:30 +0900

    Gitの練習です

---
+++ TutorialThymeleaf/src/main/java/com/techacademy/TutorialThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TutorialThymeleafApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(TutorialThymeleafApplication.class, args);
+    }
+}
